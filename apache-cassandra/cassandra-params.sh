22b9e366a68d7d2d92d7a60405e17a9f484d17bb
zinat <zinat.wali@gmail.com>
2016-05-04 03:22:53 +0100

    better paramter handling for vagrant cassandra

--- apache-cassandra/cassandra-params.sh
+++ apache-cassandra/cassandra-params.sh
@@ -1,7 +1,7 @@
 echo "exporting variables..."
-if [ "x$CASSANDRA_VERSION" == "x" ]; then export CASSANDRA_VERSION=3.2; fi
+if [ "x$CASSANDRA_VERSION" == "x" ]; then export CASSANDRA_VERSION=3.5; fi
 export CASSANDRA_TAR=apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
-if [ "x$CASSANDRA_URL" == "x" ]; then export CASSANDRA_URL=http://mirror.ox.ac.uk/sites/rsync.apache.org/cassandra/$CASSANDRA_VERSION/$CASSANDRA_TAR; fi
+if [ "x$CASSANDRA_URL" == "x" ]; then export CASSANDRA_URL=http://www-eu.apache.org/dist/cassandra/$CASSANDRA_VERSION/$CASSANDRA_TAR; fi
 if [ "x$CASSANDRA_DOWNLOAD_DIR" == "x" ]; then export CASSANDRA_DOWNLOAD_DIR=/usr/local/downloads/cassandra/cassandra-$CASSANDRA_VERSION; fi
 if [ "x$CASSANDRA_ROOT_DIR" == "x" ]; then export CASSANDRA_ROOT_DIR=/opt; fi
 
--- apache-cassandra/main.sh
+++ apache-cassandra/main.sh
@@ -1,7 +1,5 @@
 source ./cassandra-params.sh
 
-if [ "x$VAGRANT" == "x1" ]; then source ./cassandra-vagrant-params.sh; fi
-
 ./cassandra-get-files.sh
 ./cassandra-configure.sh
 ./cassandra-service.sh
