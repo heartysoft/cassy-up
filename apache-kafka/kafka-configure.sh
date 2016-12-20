d37338b3c3528ff6a31b00350faf75a0953f38be
Pierre Mage <pierre.mage@gmail.com>
2016-12-20 14:14:43 +0000

    Update Kafka version to 0.10.1.0
    Also specify the following Kafka properties:
    - num.partitions
    - log.segment.bytes
    - log.retention.bytes

--- apache-kafka/kafka-configure.sh
+++ apache-kafka/kafka-configure.sh
@@ -13,9 +13,13 @@ printf '%s\n' "source /etc/profile.d/kafka.sh" >> /etc/bash.bashrc
 sed -i "s/broker.id=0/broker.id=${KAFKA_BROKER_ID}/g" $KAFKA_DIR/config/server.properties
 sed -i "s@/tmp/kafka-logs@${KAFKA_LOG_DIR}@g" $KAFKA_DIR/config/server.properties
 sed -i "s/localhost:2181/${KAFKA_ZOOKEEPER_SERVERS}/g" $KAFKA_DIR/config/server.properties
+sed -i "s/num.partitions=1/num.partitions=${KAFKA_NUM_PARTITIONS}/g" $KAFKA_DIR/config/server.properties
+sed -i "s/log.segment.bytes=1073741824/log.segment.bytes=${KAFKA_LOG_SEGMENT_BYTES}/g" $KAFKA_DIR/config/server.properties
+
 echo 'enable.zookeeper=false' >> $KAFKA_DIR/config/server.properties
 echo "delete.topic.enable=${KAFKA_DELETE_TOPIC_ENABLE}" >> $KAFKA_DIR/config/server.properties
 echo "advertised.host.name=${KAFKA_ADVERTISED_HOST_NAME}" >> $KAFKA_DIR/config/server.properties
+echo "log.retention.bytes=${KAFKA_LOG_RETENTION_BYTES}" >> $KAFKA_DIR/config/server.properties
 
 ln -s  $KAFKA_LOG4J_DIR $KAFKA_DIR/logs
 
