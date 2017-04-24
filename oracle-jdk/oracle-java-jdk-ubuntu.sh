53d6220381a874f864bf5651327595ddca825bc1
Luis Vicente <langel.vicente@gmail.com>
2017-04-24 16:58:24 +0100

    add missing -y to Oracle JDK Ubuntu installation script.

--- oracle-jdk/oracle-java-jdk-ubuntu.sh
+++ oracle-jdk/oracle-java-jdk-ubuntu.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 echo "installing jdk 8.........................."
-add-apt-repository ppa:webupd8team/java
+add-apt-repository -y ppa:webupd8team/java
 apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install oracle-java8-installer -y
-apt-get install oracle-java8-set-default -y
+apt-get -y install oracle-java8-installer
+apt-get -y install oracle-java8-set-default
 echo "..........................jdk 8 installed."
-apt-get install libjna-java
+apt-get -y install libjna-java
 echo "..........................jna setup complete."
 
