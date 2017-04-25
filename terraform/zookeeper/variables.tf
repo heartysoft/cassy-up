b1f3b88caf26068488d0cef87d1062c389334334
Luis Vicente <langel.vicente@gmail.com>
2017-04-25 17:21:24 +0100

    add zookeeper terraform module

---
+++ terraform/zookeeper/variables.tf
@@ -0,0 +1,14 @@
+variable zookeeper_rev {}
+
+variable zookeeper_count {}
+variable zookeeper_tag_name {}
+
+variable zookeeper_ami_id {}
+variable zookeeper_instance_type {}
+variable zookeeper_iam_instance_profile {}
+variable zookeeper_subnet_ids {}
+variable zookeeper_sg_ids {}
+
+variable zookeeper_provisioning_scripts {}
+variable zookeeper_ssh_user {}
+variable zookeeper_ssh_identity_file {}
