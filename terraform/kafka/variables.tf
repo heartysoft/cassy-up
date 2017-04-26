280324fe3e61c6ed5b3973f502401d6230c52204
Luis Vicente <langel.vicente@gmail.com>
2017-04-26 18:16:52 +0100

    use lists instead of encoding them in strings.

--- terraform/kafka/variables.tf
+++ terraform/kafka/variables.tf
@@ -9,11 +9,17 @@ variable ebs_type {}
 variable ami_id {}
 variable iam_instance_profile {}
 variable instance_type {}
-variable sg_ids {}
-variable subnet_ids {}
+variable sg_ids {
+	type = "list"
+}
+variable subnet_ids {
+	type = "list"
+}
 
 variable ssh_user {}
 variable ssh_identity_file {}
 
 variable provisioning_scripts {}
-variable zk_ips {}
+variable zk_ips {
+	type = "list"	
+}
